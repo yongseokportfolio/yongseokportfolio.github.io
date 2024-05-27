bd1777ac76b44c7a3b61d372e4a9fa2249c15d9f
yongseokportfolio <jyslovly@ruu.kr>
2024-05-27 23:22:18 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -94,3 +94,36 @@ var Menu = {
 };
 
 Menu.init();
+
+$(function(){
+	// 	이미지 클릭시 해당 이미지 모달
+	$(".img-modal").click(function(){
+		$(".modal").show();
+		// 해당 이미지 가겨오기
+		var imgSrc = $(this).children("img").attr("src");
+		var imgAlt = $(this).children("img").attr("alt");
+		$(".modalBox img").attr("src", imgSrc);
+		$(".modalBox img").attr("alt", imgAlt);
+		
+		// 해당 이미지 텍스트 가져오기
+		//var imgTit =  $(this).children("p").text();
+		//$(".modalBox p").text(imgTit);
+			
+		// 해당 이미지에 alt값을 가져와 제목으로
+		//$(".modalBox p").text(imgAlt);
+	});
+		
+	//.modal안에 button을 클릭하면 .modal닫기
+	$(".modal button").click(function(){
+		$(".modal").hide();
+	});
+		
+	//.modal밖에 그리고 .modal 클릭시 닫힘
+	$(".modal").click(function (e) {
+		if (e.target.className != "modal" && e.target.className == "modal") {
+		  return false;
+		} else {
+		  $(".modal").hide();
+		}
+	});
+});
